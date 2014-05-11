69e1921edba9e62647b38d581eda479a9b06ccfb
Binocular222 <Binocular222@gmail.com>
2014-05-12 00:04:11 +0700

    Update contextual_help_xys.py

--- contextual_help_xys.py
+++ contextual_help_xys.py
@@ -2,10 +2,10 @@ import sublime, sublime_plugin, subprocess
 
 class contextual_help_xys(sublime_plugin.TextCommand):
 	def run(self, edit):
-		xypath = sublime.load_settings('XYplorer.sublime-settings').get("xypath")
+		xypath = self.view.settings().get('xypath')
 		CursorLocation = self.view.sel()[0]
 		ScopeName = self.view.scope_name(CursorLocation.a)
-		ScopeText = self.view.substr(self.view.extract_scope(CursorLocation.a)).replace("*", "")
+		ScopeText = self.view.substr(self.view.extract_scope(CursorLocation.a))
 		if "entity.name.function" in ScopeName:
 			ScopeText = ScopeText.replace("#", "_")
 			arg = xypath + "\\XYplorer.chm::/idh_scripting_comref.htm#idh_sc_" + ScopeText
@@ -13,9 +13,17 @@ class contextual_help_xys(sublime_plugin.TextCommand):
 		elif "variable.parameter" in ScopeName:
 			subprocess.Popen(["hh.exe", xypath + "\\XYplorer.chm::/idh_variables.htm"])
 		elif "keyword.control" in ScopeName:
-			subprocess.Popen(["hh.exe", xypath + "\\XYplorer.chm::/idh_scripting.htm"])
+			ScopeTextLower = ScopeText.lower()
+			if ScopeTextLower == "if" or ScopeTextLower == "elseif" or ScopeTextLower == "else":
+				subprocess.Popen(["hh.exe", xypath + "\\XYplorer.chm::/idh_scripting.htm#idh_scripting_ifthen"])
+			elif ScopeTextLower == "while":
+				subprocess.Popen(["hh.exe", xypath + "\\XYplorer.chm::/idh_scripting.htm#idh_scripting_whileloops"])
+			elif ScopeTextLower == "foreach":
+				subprocess.Popen(["hh.exe", xypath + "\\XYplorer.chm::/idh_scripting.htm#idh_scripting_foreachloops"])
+			else:
+				subprocess.Popen(["hh.exe", xypath + "\\XYplorer.chm::/idh_scripting.htm"])
 		else:
 			window.run_command("move_to", {"to": "hardeol"})        # Add new line
 			window.run_command("insert", {"characters": "\n"})      # Add new line
-
+# ::/idh_scripting_comref.htm#idh_sc_folderreport
 # mk:@MSITStore:E:\7Utilities\XYplorer\XYplorer.chm::/idh_scripting_comref.htm#idh_sc_getpathcomponent
